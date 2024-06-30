4a74aafdb70b411c8869e1b35a76da479f2d6c85
samiulalimsaad <samiulalimsaad@gmail.com>
2024-07-01 04:03:24 +0600

    env added

---
+++ utils/connectRedis.js
@@ -0,0 +1,17 @@
+const { createClient } = require("redis");
+
+const redisClient = createClient({
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+    },
+});
+
+async function connectRedis() {
+    redisClient.on("error", (err) => console.log("Redis Client Error", err));
+    await redisClient.connect();
+    console.info("Redis Connected!");
+}
+
+module.exports = { redisClient, connectRedis };
